508d2692d4e7f060e3d3b4545224541a2dd13d80
Orel S <shikher.orel@gmail.com>
2019-01-17 13:34:06 -0500

    Finally fixed filtering

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-chars = 'abcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()+|}{":?><,./;[]'
+chars = str('abcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()+|}{":?><,./;[]')
 password = ''
 passwordArray = []
 numOfPasswords = int(input("How many passwords would you like to generate?"))
@@ -7,10 +7,10 @@ counter = 1
 for p in range(numOfPasswords):
     passwordLength = float(input("How long would you like to make your password?"))
     while passwordLength <= 0 or passwordLength.is_integer() == False:  # Currently does not filter alphabetical inputs
-        passwordLength = int(input("Please enter a valid numerical length."))
-    removedCharacters = input("Which characters are disallowed?")  # Currently only allows removal of numbers and symbol
-    chars = chars.translate(None, str(removedCharacters))
-    for c in range(passwordLength):
+        passwordLength = float(input("Please enter a valid numerical length."))
+    removedCharacters = str(input("Which characters are disallowed?"))  # Currently only allows removal of numbers and symbol
+    chars = chars.translate(str.maketrans('', '',removedCharacters))
+    for c in range(int(passwordLength)):
         password += random.choice(chars)
     passwordArray.append(password)
     password = ''
@@ -18,4 +18,5 @@ print("Here are your passwords:")
 for x in passwordArray:
     print(str(counter) + ". " + x)
     counter += 1
-input()
+input("Press anything to exit...")
+
