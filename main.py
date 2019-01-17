1b4e409533ffac70ffbb6400ca91c41c0028abe8
Orel S <shikher.orel@gmail.com>
2019-01-17 14:16:00 -0500

    Fixed integer input filters
    Implemented input_checker function to check for invalid inputs for NumOfPasswords and passwordLength

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import random
+
+
+def input_checker(message):
+    while True:
+        try:
+            user_input = int(input(message))
+        except ValueError:
+            print("Not an integer! Try again.")
+            continue
+        else:
+            return user_input
+            break
+
+
 chars = str('abcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()+|}{":?><,./;[]')
 password = ''
 passwordArray = []
-numOfPasswords = int(input("How many passwords would you like to generate?"))
+numOfPasswords = input_checker("How many passwords would you like to generate?")
 counter = 1
+passwordCounter = 1
 for p in range(numOfPasswords):
-    passwordLength = float(input("How long would you like to make your password?"))
-    while passwordLength <= 0 or passwordLength.is_integer() == False:  # Currently does not filter alphabetical inputs
-        passwordLength = float(input("Please enter a valid numerical length."))
-    removedCharacters = str(input("Which characters are disallowed?"))  # Currently only allows removal of numbers and symbol
-    chars = chars.translate(str.maketrans('', '',removedCharacters))
+    passwordLength = input_checker("How long would you like password " + str(passwordCounter) + " to be?")
+    passwordCounter += 1
+    removedCharacters = str(input("Which characters are disallowed?"))
+    chars = chars.translate(str.maketrans('', '', removedCharacters))
     for c in range(int(passwordLength)):
         password += random.choice(chars)
     passwordArray.append(password)
