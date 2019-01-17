78ea76d837015102cb8917c924ec097cbae425ab
Orel S <shikher.orel@gmail.com>
2019-01-17 13:25:51 -0500

    Fixed rounding bug with float inputs

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ passwordArray = []
 numOfPasswords = int(input("How many passwords would you like to generate?"))
 counter = 1
 for p in range(numOfPasswords):
-    passwordLength = input("How long would you like to make your password?")
-    while passwordLength <= 0 or type(passwordLength) == float:  # Currently does not filter alphabetical inputs
+    passwordLength = float(input("How long would you like to make your password?"))
+    while passwordLength <= 0 or passwordLength.is_integer() == False:  # Currently does not filter alphabetical inputs
         passwordLength = int(input("Please enter a valid numerical length."))
-    print(passwordLength)
-    removedCharacters = input("Which characters are disallowed?")  # Currently only allows removal of numbers and symbols
+    removedCharacters = input("Which characters are disallowed?")  # Currently only allows removal of numbers and symbol
     chars = chars.translate(None, str(removedCharacters))
     for c in range(passwordLength):
         password += random.choice(chars)
