faf52826b7c917edc8019ab69df9b9f7ffb47153
Orel S <shikher.orel@gmail.com>
2019-01-17 19:25:15 -0500

    Fixed removing all characters bug

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ def wait_for_input():
     visualc.getch()
 
 
+def remove_repeat_characters(message):
+    message = ''.join(
+        [char for index, char in enumerate(message) if char not in message[0:index]])
+    return message
+
+
 def input_checker(message):
     while True:
         try:
@@ -28,6 +34,9 @@ for p in range(numOfPasswords):
     passwordLength = input_checker("How long would you like password " + str(numOfPasswordCounter) + " to be?")
     numOfPasswordCounter += 1
     removedCharacters = str(input("Which characters are disallowed?"))
+    while chars.translate(str.maketrans('', '', removedCharacters)) == '':
+        print("You may not disallow all characters. Please try again.")
+        removedCharacters = str(input("Which characters are disallowed?"))
     chars = chars.translate(str.maketrans('', '', removedCharacters))
     for c in range(int(passwordLength)):
         password += random.choice(chars)
@@ -38,4 +47,7 @@ for x in passwordArray:
     print(str(passwordConstructCounter) + ". " + x)
     passwordConstructCounter += 1
 wait_for_input()
+#add fix for if input all of chars, just a quick if check
+#put removed char versions into an array
+#add file i/o
 
